550dc213035c270ede59d898137873f55ea96be4
Brandon P <brandonman17@gmail.com>
2020-09-18 22:33:59 -0400

    feat: more setup again

--- controller.js
+++ controller.js
@@ -1,3 +1,5 @@
+const int SQUARE_SIZE = 10;
+
 $(document).ready(function() {
     const canvas = document.querySelector('canvas'); 
     const ctx = canvas.getContext('2d'); 
@@ -12,3 +14,19 @@ $(document).ready(function() {
     ctx.fillStyle = 'green'; 
     ctx.fillRect(0, 0, 300, 500);
 }*/
+
+function drawGrid(int[][] gridArray) {
+    
+    for(int row=0; row<gridArray.Count(); row++) {
+        for(int col=0; col<gridArray[row].Count(); col++) {
+            drawSquare(row * SQUARE_SIZE, col * SQUARE_SIZE, gridArray[row][col]);
+        }
+    }
+}
+
+function drawSquare(int x, int y, String color) {
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
+}
