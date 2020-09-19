b492c286ff1b0dd02ba98fac6e5c052e489e338c
KacperWojtowicz <71534241+KacperWojtowicz@users.noreply.github.com>
2020-09-19 01:03:44 -0400

    Revert "feat: falling square"
    This reverts commit 6c2533af62d37acde0185cefe5a9013324067edc.

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
-//const SQUARE_SIZE = 10;
+const int SQUARE_SIZE = 10;
 
-/*$(document).ready(function() {
+$(document).ready(function() {
     const canvas = document.querySelector('canvas'); 
     const ctx = canvas.getContext('2d'); 
     ctx.fillStyle = 'green'; 
@@ -8,12 +8,12 @@
     //while(true) {mainLoop();}
 });
 
-function mainLoop() {
+/*function mainLoop() {
     const canvas = document.querySelector('canvas'); 
     const ctx = canvas.getContext('2d'); 
     ctx.fillStyle = 'green'; 
     ctx.fillRect(0, 0, 300, 500);
-}
+}*/
 
 function drawGrid(int[][] gridArray) {
     
@@ -29,29 +29,4 @@ function drawSquare(int x, int y, String color) {
     const ctx = canvas.getContext('2d');
     ctx.fillStyle = color;
     ctx.fillRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
-}*/
-
-var canvas, ctx, x, last;
-
-$(document).ready(function() {
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    x = 0;
-    last = performance.now();
-});
-
-
-
-function draw(timestamp) {
-  requestAnimationFrame(draw);
-  
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.rect(150, x, 20, 20);
-  ctx.fillStyle = "#ffffff";
-  ctx.fill();
-  
-  x += (timestamp - last) / 10;
-  last = timestamp;
 }
-requestAnimationFrame(draw);
