d1cd550ce648674af031a6dbe91d32563b0086e5
Brandon P <brandonman17@gmail.com>
2020-09-18 21:39:08 -0400

    feat: more setup

--- controller.js
+++ controller.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
     const ctx = canvas.getContext('2d'); 
     ctx.fillStyle = 'green'; 
     ctx.fillRect(0, 0, 300, 500);
+    //while(true) {mainLoop();}
 });
+
+/*function mainLoop() {
+    const canvas = document.querySelector('canvas'); 
+    const ctx = canvas.getContext('2d'); 
+    ctx.fillStyle = 'green'; 
+    ctx.fillRect(0, 0, 300, 500);
+}*/
